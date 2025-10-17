aa7c6c490bdd5720acb014aed68be1fa1dce94f2
Cornelius <cornelius.denninger@gmail.com>
2025-10-17 10:35:32 +0200

    Init

---
+++ index.ts
@@ -0,0 +1,10 @@
+/**
+ * This functions takes in a unfinished or complete json string and returns a json object.
+ *
+ * @param input
+ *
+ * @returns
+ */
+export const zerteiler = (input: string) => {
+  console.log(`Zerteiler: ${input}`);
+};
